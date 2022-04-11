a99b1ef5bf61caa72ca14624e8aefea5a80afc48
Z9tails <100103488+Z9tails@users.noreply.github.com>
2022-04-10 19:09:27 -0700

    git repair

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+    // Variable array of possible strings 
+
+    var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var charArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+    // Empty array path
+
+    var resultArray = [];
+    var userArray = [];
+
+    uppercaseArray[1]
+    //-----------------------------------------------------------------
+    var numCharacter = prompt("How long of a password do you want (between 1 and 128 characters?");
+    var numbers = confirm("Would you like numbers in your password?");
+    var uppercases = confirm("Would you like Uppercase letters in your password?");
+    var lowercases = confirm("Would you like lowercase letters in your password?");
+    var characters = confirm("And lastly, any special characters in your password?");
+
+
+    // "If" statements with concat instead of spread or push, spread will be used far more often in the future, but I wasn't quite able to manage the process.
+
+
+    if (numbers) {
+        resultArray = resultArray.concat(numArray);
+
+    }
+
+    if (uppercases) {
+        resultArray = resultArray.concat(uppercaseArray);
+
+    }
+
+    if (lowercases) {
+        resultArray = resultArray.concat(lowercaseArray);
+
+    }
+
+    if (characters) {
+        resultArray = resultArray.concat(charArray);
+    }
+    console.log(resultArray)
+
+
+    for (var i = 0; i < numCharacter; i++) {
+
+        userArray.push(resultArray[Math.floor(Math.random() * resultArray.length)]);
+    }
+
+    return userArray.join("");
+}
+
+// Display password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+generateBtn.addEventListener("click", writePassword);
+
+function timedRefresh(timeoutPeriod) {
+    setTimeout("location.reload(true);", timeoutPeriod);
+}
+
+window.onload = timedRefresh(120000);
